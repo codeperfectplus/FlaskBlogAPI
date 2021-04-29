35ed54d57d71eca8c70a19c2a78685ea714fd59e
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2021-04-29 22:24:52 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-blog_title = "create new appplication"
-random_words =
-
-blog_slug = blog_title.replace(' ', '-') + "-" + ''.join(random.choice(string.ascii_lowercase) for _ in range(4))
-print(blog_slug)
